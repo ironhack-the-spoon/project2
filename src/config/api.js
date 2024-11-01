b4c5138f41b0253af06ecc71116dc992f3474e82
“Samy <samy.mouahbi@hotmail.com>
2024-11-01 18:47:47 +0100

    create list of restaurants on the home page

---
+++ src/config/api.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://the-spoon-a916a-default-rtdb.europe-west1.firebasedatabase.app"
--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
-
+import  { useState, useEffect } from 'react'
+import axios from "axios";
+import { API_URL } from "../config/api";
+import { Link } from "react-router-dom";
 function Restaurants() {
+
+  const [ restaurantsList, setRestaurantsList] = useState([]);
+
+  console.log(restaurantsList)
+
+    useEffect(() => {
+      axios.get(`${API_URL}/projects.json`)
+      .then(response => {
+        setRestaurantsList(response.data["-OAcoLpdhxVFn7lPXMMf"])
+      })
+      .catch(e => console.log("Error getting projects from the API...", e));    
+    }, []);
+
+    if (restaurantsList === null) {
+      return "Load..."
+    }
+
   return (
-    <div>Restaurants</div>
+    <div>Restaurants
+      {restaurantsList && restaurantsList.map(restaurantDetails => {
+        return (
+          <div className="card" key={restaurantDetails.id}>
+          <Link to={`/details/${restaurantDetails.id}`}>
+              <h3>{restaurantDetails.name}</h3>
+          </Link>
+      </div>
+        );
+      })}
+    </div>
   )
 }
 
