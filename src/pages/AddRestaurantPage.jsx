85fe6ce208d8b9ad66cf72cb5ca1afd6d8986a8e
Bopally <estelleb0707@gmail.com>
2024-11-06 17:54:12 +0100

    Add marging to the add restaurant form

--- src/pages/AddRestaurantPage.jsx
+++ src/pages/AddRestaurantPage.jsx
@@ -2,7 +2,7 @@ import AddRestaurantForm from "../components/AddRestaurantForm";
 
 function AddRestaurantPage() {
   return (
-    <div className="max-w-md mx-auto p-6 bg-green-700 shadow-md rounded-lg">
+    <div className="max-w-md mx-auto mt-12 mb-12 p-6 bg-green-700 shadow-md rounded-lg">
       <h1 className="text-4xl font-bold text-beige-light mb-6 text-center">
         Spill the beans about your restaurant!
       </h1>
