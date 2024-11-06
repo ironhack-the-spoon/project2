09b386ff191bb00992db3198ef36be2efcf5f752
“Samy <samy.mouahbi@hotmail.com>
2024-11-06 16:46:21 +0100

    Commit before merge with Estelle's code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        title: ["Meera Inimai","sans-serif"]
+      },
+    },
   },
   plugins: [],
 };
