822641e15c898e266cc0292112013a323204345d
“Samy <samy.mouahbi@hotmail.com>
2024-11-06 19:41:45 +0100

    Create a map using iframe on the home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        perso: ['Raleway', 'sans-serif'],
+      },
       colors: {
-        customGreen: "#00582B",
+        customGreen: '#06402b', // Add your custom color here
         green: {
           600: "#00582B",
           700: "#004A24",
