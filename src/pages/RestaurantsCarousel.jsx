6c0eff97ccd021767035ea7538e5806259d3d23b
Bopally <estelleb0707@gmail.com>
2024-11-06 21:30:22 +0100

    Solve bug when adding a new restaurant

--- src/pages/RestaurantsCarousel.jsx
+++ src/pages/RestaurantsCarousel.jsx
@@ -11,20 +11,35 @@ function RestaurantsCarousel({ selectedType }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    axios
-      .get(`${API_URL}/projects.json`)
-      .then((response) => {
+    const fetchRestaurants = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/projects.json`);
         const allRestaurants = Object.keys(response.data).map((key) => ({
           ...response.data[key],
           id: key,
         }));
-        const filteredRestaurants = allRestaurants.filter(
-          (restaurant) => restaurant.type === selectedType
-        );
+        console.log("All Restaurants:", allRestaurants);
+
+        const filteredRestaurants = allRestaurants.filter((restaurant) => {
+          const isMatchingType =
+            restaurant.type &&
+            restaurant.type.toLowerCase() === selectedType.toLowerCase();
+          console.log(
+            `Restaurant ${restaurant.name} isMatchingType:`,
+            isMatchingType
+          );
+          return isMatchingType;
+        });
+        console.log("Filtered Restaurants:", filteredRestaurants);
+
         setRestaurantsList(filteredRestaurants);
         setCurrentIndex(0);
-      })
-      .catch((e) => console.log("Error getting projects from the API...", e));
+      } catch (e) {
+        console.log("Error getting projects from the API...", e);
+      }
+    };
+
+    fetchRestaurants();
   }, [selectedType]);
 
   const next = () => {
@@ -50,7 +65,7 @@ function RestaurantsCarousel({ selectedType }) {
       <div className="carousel">
         {restaurantsList
           .slice(currentIndex, currentIndex + 3)
-          .map((restaurant, index) => (
+          .map((restaurant) => (
             <Link
               to={`/details/${restaurant.id}`}
               key={restaurant.id}
