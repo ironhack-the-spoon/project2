c1c6f9ddd7f75771b786eb54a13c2a3459ab098d
Bopally <estelleb0707@gmail.com>
2024-11-07 18:27:40 +0100

    Solve bug with images during deployment V2

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -1,11 +1,9 @@
+import Team from "../assets/images/about-team.png";
+
 function OurTeam() {
   return (
     <section className="about-team mb-8 p-6 bg-white border border-gray-200 rounded-lg shadow-lg flex flex-col items-center text-center">
-      <img
-        src="../assets/images/about-team.png"
-        alt="Team Icon"
-        className="w-16 h-16 mb-4"
-      />
+      <img src={Team} alt="Team Icon" className="w-16 h-16 mb-4" />
       <h2 className="text-3xl font-semibold mb-4 text-customGreen">Our Team</h2>
       <p className="text-lg leading-relaxed">
         Our team is composed of passionate food enthusiasts and tech experts
